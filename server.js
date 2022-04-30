4ee234109415b363358912a2453da6f8d63c516f
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-04-29 23:59:01 -0500

    refactor files and add employee ALL option

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 
-const { example1 } = require("./lib/roles");
 
-const menu = require('./lib/inquirer');
+
+const menuStart = require('./lib/menu');
 
 const console = require('console');
   
@@ -18,120 +18,48 @@ console.log(
 
 
 
-function consoleTable(data) {
-    const cTable = require('console.table');
-    const table = cTable.getTable(data);
-    console.log(table);
-}
+// function consoleTable(data) {
+//     const cTable = require('console.table');
+//     const table = cTable.getTable(data);
+//     console.log(table);
+// }
 
-async function getDepartments () {
-    const mysql = require('mysql2/promise');
-    // get the promise implementation, we will use bluebird
-    const bluebird = require('bluebird');
-    const db = await mysql.createConnection(
-            {
-                host: 'localhost',
-                user: 'root',
-                password: 'Sql2022',
-                database: 'employee_tracker',
-                Promise: bluebird
-            },
-            console.log('connected to the election database.')
-        );
 
-    const sql = `SELECT * FROM departments`;
+// async function getRoles() {
     
-    const [departments, fields]  = await db.execute(sql);
+//     const mysql = require('mysql2/promise');
+//     // get the promise implementation, we will use bluebird
+//     const bluebird = require('bluebird');
+//     const db = await mysql.createConnection(
+//         {
+//             host: 'localhost',
+//             user: 'root',
+//             password: 'Sql2022',
+//             database: 'employee_tracker',
+//             Promise: bluebird
+//         },
+//         console.log('connected to the election database.')
+//     );
+
+//     const sql = `SELECT 
+//                         r.id AS Id,
+//                         r.title AS Title,
+//                         r.salary AS Salary,
+//                         d.name AS Department
+//                    FROM roles r
+//                    LEFT JOIN departments d
+//                    ON r.department_id = d.id`;
+
+//     const [roles, fields]  = await db.execute(sql);
     
-    db.end();
-  
-    return departments;
+//    // console.table(roles);
 
-}
+//    consoleTable(roles)
 
-async function getRoles() {
-    
-    const mysql = require('mysql2/promise');
-    // get the promise implementation, we will use bluebird
-    const bluebird = require('bluebird');
-    const db = await mysql.createConnection(
-        {
-            host: 'localhost',
-            user: 'root',
-            password: 'Sql2022',
-            database: 'employee_tracker',
-            Promise: bluebird
-        },
-        console.log('connected to the election database.')
-    );
+//     db.end();
 
-    const sql = `SELECT 
-                        r.id AS Id,
-                        r.title AS Title,
-                        r.salary AS Salary,
-                        d.name AS Department
-                   FROM roles r
-                   LEFT JOIN departments d
-                   ON r.department_id = d.id`;
-
-    const [roles, fields]  = await db.execute(sql);
-    
-   // console.table(roles);
-
-   consoleTable(roles)
-
-    db.end();
-
-}
+// }
  
 
-//getRoles ()
-//menu();
 
-
-const inquirer = require("inquirer");
-
-function menuStart() {
-    
-    const menu = [
-        "View All Departments",
-        "View All Roles",
-        "View All Employees",
-        "Add Department",
-        "Add Role",
-        "Add Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-        "View All Employees By Manager",
-        "View All Employees By Department",
-        "Delete Department",
-        "Delete Role",
-        "Delete Employee",
-        "Exit"
-    ];
-
-    inquirer.prompt(
-        {
-            type: "list",
-            name: "menu",
-            message: "What would you like to do?",
-            choices: menu
-        }).then(answers => {
-            switch (answers.menu) {
-                case 'View All Departments':
-                    getDepartments().
-                        then(departments => { consoleTable(departments); menuStart()   });
-                     
-                    break;
-            }
-
-        
-            console.log(answers.menu)
-            
-        })
-            .catch(err => {
-                console.log(err);
-            });
-
-}
 menuStart();
